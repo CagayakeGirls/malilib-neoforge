0f01708a8e769fd0b00de153b8b45530b4383efc
TexTrue <65154269+TexBlock@users.noreply.github.com>
2025-12-20 12:40:18 +0800

    try fix #9

--- src/main/java/team/cagayakegirls/mafglib/utils/ModPlatform.java
+++ src/main/java/team/cagayakegirls/mafglib/utils/ModPlatform.java
@@ -24,7 +24,9 @@ public class ModPlatform {
     }
 
     public static List<IModInfo> getAllMods() {
-        return ModList.get().getMods();
+        return FMLLoader.getCurrent().getLoadingModList().getMods().stream()
+                        .map(modInfo -> (IModInfo) modInfo)
+                        .toList();
     }
 
     public static Optional<? extends ModContainer> getModContainer(String modId) {
