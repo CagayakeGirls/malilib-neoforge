c757f71c4c2ab6a3dcfb312c35d35bfb2432255c
TexTrue <65154269+TexBlock@users.noreply.github.com>
2026-04-08 20:20:53 +0800

    clean

--- src/main/java/team/cagayakegirls/mafglib/render/pip/MaLiLibGuiElementRendererRegistries.java
+++ src/main/java/team/cagayakegirls/mafglib/render/pip/MaLiLibGuiElementRendererRegistries.java
@@ -4,7 +4,9 @@ import fi.dy.masa.malilib.render.special.MaLiLibBlockStateGuiElement;
 import fi.dy.masa.malilib.render.special.MaLiLibBlockStateGuiElementRenderer;
 import net.minecraft.client.renderer.block.BlockModelResolver;
 import net.neoforged.neoforge.client.gui.PictureInPictureRendererRegistration;
+import org.jetbrains.annotations.ApiStatus;
 
+@ApiStatus.Experimental
 public class MaLiLibGuiElementRendererRegistries {
     public static PictureInPictureRendererRegistration<MaLiLibBlockStateGuiElement> blockStateGuiElement(BlockModelResolver blockModelResolver) {
         return new PictureInPictureRendererRegistration<>(MaLiLibBlockStateGuiElement.class, (bufferSource) -> new MaLiLibBlockStateGuiElementRenderer(bufferSource, blockModelResolver));
